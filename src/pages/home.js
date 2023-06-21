cc19849620dbfb1a1a9a27492962fdbe2da2562e
Mr-Deee <merchantdaniel8@gmail.com>
2023-06-21 18:56:45 +0000

    root for image

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,34 @@
 import React from "react";
 import { AiOutlineHome } from "react-icons/ai";
-
+import './home.css'
 function Home() {
   return (
+
+
+    <>
+
+    <div className="Welcome">
+    <div >
+    <h1 className="texthi">Hi there,<span class="text-highlight">Welcome</span></h1>
+                <p>Dash, home for all your checkins</p>
+
+                <div className="image-container">
+      <img src="path/to/your/image.jpg" alt="Scrollable Image" className="scrollable-image" />
+    </div>
+                
+  </div>
+
+  
+    
+    </div>
+
+
+
     <div className="home">
       <h1>Home</h1>
       <AiOutlineHome className="page-icon" />
     </div>
+    </>
   );
 }
 
